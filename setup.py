ae6dd5abfbde381b2d74dec83cf73ce861029388
James Thornton <james.thornton@gmail.com>
2011-08-07 17:27:37 -0500

    Bump version number to 0.2.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def run_tests():
 
 setup (
     name = 'bulbs',
-    version = '0.2-dev',
+    version = '0.2.1',
     url = 'http://bulbflow.com',
     license = 'BSD',
     author = 'James Thornton',
