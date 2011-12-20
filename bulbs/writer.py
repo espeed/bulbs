27b866456605b131b6746b0247b2cf2b9b951b1b
James Thornton <james.thornton@gmail.com>
2011-12-20 10:31:52 -0600

    intial github push

--- bulbs/writer.py
+++ bulbs/writer.py
@@ -124,3 +124,26 @@ class ScriptWriter(object):
             keys = data.keys()
         return keys
 
+
+
+#
+# Example Usage
+#
+def create_and_index_vertex(index_name,data,keys=None):
+    # We won't have to do it this way when Gremlin methods are overloaded
+    # to accept JSON strings in place of Groovy maps. Issue filed on GitHub.
+    s = ScriptWriter()
+    s.start_transaction()
+    s.add_vertex("v")
+    s.set_property_data("v",data)
+    s.get_index("i",index_name,"Vertex")
+    keys = s.get_keys(data,keys)
+    for key, value in data.items():
+        if key in keys:
+            s.index_put("i",key,value,"v")
+    s.end_transaction()
+    # put the return statement after end_transaction else it won't commit
+    s.return_var("v") 
+    script = s.get()
+    #print s.display()
+    return script
