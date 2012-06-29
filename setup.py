a40b9e6c8bbfc45e6ca6ed28aac5cc3e1024a77e
James Thornton <james.thornton@gmail.com>
2012-06-29 11:07:21 -0500

    bumped version to 0.3.1

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ else:
 
 setup (
     name = 'bulbs',
-    version = '0.3',
+    version = '0.3.1',
     url = 'https://github.com/espeed/bulbs',
     license = 'BSD',
     author = 'James Thornton',
