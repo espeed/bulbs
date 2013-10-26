c4740e38f5cfff7c217efb93d2a5da8ed32c117c
James Thornton <james.thornton@gmail.com>
2013-10-26 18:36:32 -0500

    remove groovy.refresh namespace arg

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ else:
 
 setup (
     name = 'bulbs',
-    version = '0.3.19',
+    version = '0.3.20',
     url = 'https://github.com/espeed/bulbs',
     license = 'BSD',
     author = 'James Thornton',
